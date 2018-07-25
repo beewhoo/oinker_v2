4826d2706dfbf7aa9d8ee0018ff3785ef9392712
YUMEE <yumeehk@hotmail.com>
2018-07-25 12:02:36 -0400

    Routes and welcome controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  
+
   resources :events
   resources :restaurants
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :restaurants
-  root 'restaurants#index'
+  root 'welcome#homepage'
+
+  get '/oinker', to: 'welcome#homepage'
+  get '/oinker/carousel', to: 'welcome#carousel'
+  get '/oinker/choice', to: 'welcome#res_ev_choice'
+  get '/oinker/date_choice', to: 'welcome#date_choice'
+  get '/oinker/budget_choice', to: 'welcome#budget_choice'
+  get '/oinker/location_choice', to: 'welcome#location_choice'
+  get '/oinker/party_size', to: 'welcome#party_size'
+  get '/oinker/categories', to: 'welcome#categories'
 end
