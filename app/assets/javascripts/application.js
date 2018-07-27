6de33c56d1f8dd54ced957b8088fa7a9c1e70489
YUMEE <yumeehk@hotmail.com>
2018-07-27 11:54:15 -0400

    Compiled choices html.erb files into one select_choices.html.erb file. Added JS code to the selectChoice.js file to work with budget and date choice.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,17 +23,5 @@ document.addEventListener('DOMContentLoaded', function(){
 
 var checkList = document.getElementById('catlist')
 
-if (checkList !== null) {
-var categories = document.querySelector('.categories')
-checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
-  if (categories.classList.contains('visible')){
-    categories.classList.remove('visible');
-  }
-  else {
-    categories.classList.add('visible');
-  }}
-  checkList.onblur = function(evt) {
-    categories.classList.remove('visible');
-  }
-}
+
 });
---
+++ app/assets/javascripts/selectChoices.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+
+// DATE CHOICE
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );
+
+// BUDGET CHOICE
+$( function() {
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 100,
+    values: [ 25, 75 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
+
+// CATEGORY CHOICE
+if (checkList !== null) {
+var categories = document.querySelector('.categories')
+checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
+  if (categories.classList.contains('visible')){
+    categories.classList.remove('visible');
+  }
+  else {
+    categories.classList.add('visible');
+  }}
+  checkList.onblur = function(evt) {
+    categories.classList.remove('visible');
+  }
+}
+
+
+
+});
