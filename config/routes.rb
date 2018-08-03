793fa108778c9950554b07abc5a29d7f969c7285
w0nd3r0us <steven.grewal@gmail.com>
2018-08-02 20:18:59 -0400

    added mailers for date confirmation, customized 404 page, created about page, tweaked routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
   get '/welcome', to: 'welcome#homepage'
   get '/welcome/choice', to: 'welcome#select_choices'
   get '/welcome/trending', to: 'welcome#trending'
+  get '/welcome/about', to: 'welcome#about'
   get '/date_plan', to: 'date_plan#plan'
   post '/date_plan', to: 'date_plan#create'
   get '/date_plan/:id', to: 'date_plan#show', as: 'show_date_plan'
   delete '/date_plan/:id', to: 'date_plan#destroy'
   get '/user', to: 'user#show'
+
 end
