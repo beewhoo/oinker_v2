8f504d039a9c1be2880536660d956efd461324ef
beewhoo <bibeksinghtthapa@gmail.com>
2018-08-03 02:45:59 -0400

    deleted method in user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'notifications@oinker.com'
 
   def saved_plan_email
-    @date_plans = @date_plan = DatePlan.find(params[:id])
-    mail(to: @date_plan.user_id.email, 'Here are your Oinker plan details!')
+    # @date_plans = @date_plan = DatePlan.find(params[:id])
+    #
+    # # mail(to: @date_plan.user_id.email, 'Here are your Oinker plan details!')
   end
 end
